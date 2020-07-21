0942911cb58da07b10a10d534c9b96953f7ca64f
jason <wangzepeng@appropolis.com.cn>
2020-07-21 15:13:54 +0800

    init

---
+++ src/components/common/treeTable.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="tree-table">
+    <div v-for="(item,index) in treeData" :key="item.name">
+      <div :style="{width:wrapWidth+'px'}">{{item.name}}</div>
+      <template v-if="item.children && item.children.length">
+        <tree-table :tree-data="item.children" :wrapWidth="wrapWidth"></tree-table>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'tree-table',
+    data() {
+      return {
+
+      }
+    },
+    props: {
+      //其容器元素的尺寸/列数=每列宽度
+      wrapWidth: {
+        default: 0
+      },
+      treeData: {
+        default () {
+          //最终数据结构预览
+          return [
+          /*  
+            {
+            name: 'JOBS',
+            children: [{
+                name: '2018-01',
+                children: [{
+                    name: '2018-01-01',
+                    children: [{
+                      name: 12,
+                      children: [{
+                        name: 16
+                      }]
+                    }]
+                  },
+                  {
+                    name: '2018-01-07',
+                    children: [{
+                      name: 12,
+                      children: [{
+                        name: 16
+                      }]
+                    }]
+                  },
+                ]
+              },
+              {
+                name: '2018-02',
+                children: [{
+                    name: '2018-01-08',
+                    children: [{
+                      name: 12,
+                      children: [{
+                        name: 16
+                      }]
+                    }]
+                  },
+                  {
+                    name: '2018-01-09',
+                    children: [{
+                      name: 12,
+                      children: [{
+                        name: 16
+                      }]
+                    }]
+                  },
+                ]
+              },
+            ]
+          }, {
+            name: 'JOBS1',
+            children: [{
+                name: '2018-01',
+                children: [{
+                    name: '2018-01-01',
+                    children: [{
+                      name: 12,
+                      children: [{
+                        name: 16
+                      }]
+                    }]
+                  },
+                  {
+                    name: '2018-01-07',
+                    children: [{
+                      name: 12,
+                      children: [{
+                        name: 16
+                      }]
+                    }]
+                  },
+                ]
+              },
+              {
+                name: '2018-02',
+                children: [{
+                    name: '2018-01-08',
+                    children: [{
+                      name: 12,
+                      children: [{
+                        name: 16
+                      }]
+                    }]
+                  },
+                  {
+                    name: '2018-01-09',
+                    children: [{
+                      name: 12,
+                      children: [{
+                        name: 16
+                      }]
+                    }]
+                  },
+                ]
+              },
+            ]
+          }*/
+          ]
+        }
+      }
+    },
+    mounted() {
+    }
+  }
+</script>
+
+<style>
+  .tree-table>div {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    box-shadow: inset 0 0 0 1px #000;
+  }
+  .tree-table>div+div {
+    margin-top: -1px;
+  }
+
+  .tree-table>div>div:first-child {
+    /* 显示文字的dom元素 */
+    padding:5px 8px;
+    box-sizing: border-box;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .tree-table div:nth-child(n+1) {
+    /* 剩余项填充满除第一项宽度的剩余宽度 */
+    flex: 1
+  }
+</style>
---
+++ src/components/page/table.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="page-wrap">
+    <div ref="table-box" class="table-box">
+      <tree-table  :treeData="result" :wrapWidth="tableBoxWidthComputed"></tree-table>
+    </div>
+  </div>
+</template>
+
+<script>
+  import treeTable from '../common/treeTable.vue';
+  export default {
+    name: 'dashboard',
+    data() {
+      return {
+        result: [],
+        data: [
+          ['JOBS', '2018-01', '2018-01-01', 12, 16],
+          ['JOBS', '2018-01', '2018-01-07', 13, 17],
+          ['JOBS', '2018-02', '2018-01-08', 14, 18],
+          ['JOBS', '2018-02', '2018-01-09', 15, 19],
+          ['JOBS', '2018-02', '2018-01-12', 16, 10],
+          ['JOBS', '2018-03', '2018-01-15', 8, 11],
+          ['JOBS', '2018-03', '2018-01-16', 6, 12],
+          ['JOBS', '2018-03', '2018-01-18', 8, 13],
+          ['JOBS', '2018-03', '2018-01-21', 9, 14],
+          ['JOBS', '2018-04', '2018-01-23', 12, 15],
+          ['JOBS', '2018-04', '2018-01-26', 9, 16],
+          ['JOBS', '2018-05', '2018-01-30', 12, 17],
+          ['JOBS1', '2018-01', '2018-01-01', 12, 18],
+          ['JOBS1', '2018-01', '2018-01-07', 12, 19],
+          ['JOBS1', '2018-02', '2018-01-08', 12, 20],
+          ['JOBS1', '2018-02', '2018-01-09', 12, 21],
+          ['JOBS1', '2018-02', '2018-01-12', 12, 22],
+          ['JOBS1', '2018-03', '2018-01-15', 12, 23],
+          ['JOBS1', '2018-03', '2018-01-16', 12, 24],
+
+        ],
+        typeList: ['服务', '周', '日'],
+        valueType: ['平均值', '最大值'],
+        tableBoxWidth:0
+      };
+    },
+    components: {
+      treeTable
+    },
+    computed: {
+      headerList(){
+        return [...this.typeList,...this.valueType]
+      },
+      tableBoxWidthComputed() {
+        return this.tableBoxWidth / this.headerList.length
+      },
+      
+
+    },
+    created() {
+      //合并表头数据
+      this.data.unshift(this.headerList)
+      //遍历数据
+      this.data.forEach((v) => {
+        //递归转化数据
+        this.diguiFn(v, this.result)
+      })
+    },
+    mounted() {
+      //
+      this.tableBoxWidth = this.$refs['table-box'].clientWidth;
+      window.addEventListener('resize',()=>{
+        this.tableBoxWidth = this.$refs['table-box'].clientWidth;
+      })
+    },
+
+    methods: {
+      diguiFn(data, arr) {
+        //如果不存在
+        if (!arr.find((w, j) => {
+            return w.name && w.name === data[0]
+          })) {
+          let obj = {
+            name: data[0],
+            children: []
+          }
+          //如果data.slice(1)中还有元素则一直递归下去
+          if (data.slice(1).length) {
+            this.diguiFn(data.slice(1), obj.children)
+          }
+          // 遍历完毕整体填充至arr中
+          arr.push(obj)
+        } else {//数据中已存在
+          let index = arr.findIndex((w, j) => {
+            return w.name === data[0]
+          })
+          if (data.slice(1).length) {
+            this.diguiFn(data.slice(1), arr[index].children)
+          }
+        }
+      }
+    }
+  };
+</script>
+
+<style scoped>
+.page-wrap {
+  padding:10px;
+}
+.table-box {
+  background:rgba(0,0,0,.3);
+}
+</style>
